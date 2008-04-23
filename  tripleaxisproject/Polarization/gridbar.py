12768b9c7e90ded1ee63ae509280d99915998c19
william.ratcliff <william.ratcliff@e28a235e-f944-0410-a937-4d0c1e564b32>
2008-04-23 22:01:48 +0000

    fixed cataloging for polarized beam

---  tripleaxisproject/Polarization/gridbar.py
+++  tripleaxisproject/Polarization/gridbar.py
@@ -1,12 +1,30 @@
 import wx
 import wx.grid
 import copy
+import numpy as N
+from pickle import loads,dumps
+threshold=1e-3
+
+class Range:
+    def __init__(self,myrange):
+        self.low=myrange[0]#900
+        self.high=myrange[1]#-900
+    def map(self,value):
+        if self.low>=self.high:
+            return 0.5
+        else:
+            return (value-self.low)/(self.high-self.low)
+
 
 class Bar:
-    def __init__(self,low,high,range):
+    def __init__(self,low,high,myrange):
         self.low=low
         self.high=high
-        self.range=range
+        self.range=myrange
+
+
+    def __str__(self):
+        return "Bar(%g,%g)"%(self.low,self.high)
 
 GRID_VALUE_BAR=Bar.__name__
 
@@ -18,32 +36,61 @@ class GridCellBarRenderer(wx.grid.PyGridCellRenderer):
         wx.grid.PyGridCellRenderer.__init__(self,*args,**kwargs)
         print 'hello'
 
-    def Draw(self, grid, attr, dc, rect, row, col, isSelected):
+    def DrawBar(self, grid, attr, dc, rect, row, col, isSelected):
         print 'Drawing'
-        range = grid.GetCellValue(row, col)
-        #dc=wx.DC
-        dc.Clear()
-        #dc.BeginDrawing()
+        #print str(grid.GetCellValue(row, col))
+        #bar = loads(str(grid.GetCellValue(row, col)))
+        #bar = Bar.getcell(int(grid.GetCellValue(row,col)))
+        table=grid.GetTable()
+        bar = table.GetValue(row,col)
+        print 'bar ',bar
+        if bar=='':
+            return
+        low=bar.low
+        high=bar.high
+        bar_range=bar.range
+        print 'low',bar.low,'high',bar.high,' range', bar_range
         cell_height=rect.height
         cell_width=rect.width
         cell_x=rect.x
         cell_y=rect.y
         rect.height=rect.height/4
-        rect.width=rect.width/3
         rect.y=rect.y+2*rect.height
-        rect.x=cell_x+cell_width/4
+        rect_b=copy.deepcopy(rect)
+        dc.SetBrush(wx.Brush("white",wx.SOLID))
+        dc.SetPen(wx.Pen("navy"))
+        dc.DrawRectangleRect(rect_b)
+        rangefinder=Range(bar_range)
+        low_mapped=rangefinder.map(low)
+        high_mapped=rangefinder.map(high)
+        rect.width=(high_mapped-low_mapped)*cell_width
+        if rect.width<threshold:
+            rect.width=cell_width/10
+        rect.x=cell_x+low_mapped*rect.width
+        print 'low_mapped',low_mapped,'high_mapped',high_mapped,'width',rect.width,'rect.x',rect.x
         dc.SetBrush(wx.Brush("navy",wx.SOLID))
         dc.SetPen(wx.Pen("navy"))
         dc.DrawRectangleRect(rect)
-        dc.DrawText('h',cell_x,cell_y)
-        print rect.x, rect.y
-        #dc.EndDrawing()
-        print rect
+        s1='%4.3f'%(low,)
+        s2='%4.3f'%(high,)
+        dc.SetFont(wx.Font(7, wx.DEFAULT, wx.NORMAL, wx.NORMAL))
+        text_width, text_height = dc.GetTextExtent(s2)
+
+        #dc.DrawText(s1,cell_x,cell_y)
+        #dc.DrawText(s2,cell_x+cell_width-text_width,cell_y)
+
+    def Draw(self, grid, attr, dc, rect, row, col, isSelected):
+
+        dc.SetBrush(wx.Brush("white",wx.SOLID))
+        dc.SetPen(wx.Pen("white"))
+        dc.DrawRectangleRect(rect)
+        self.DrawBar(grid, attr, dc, rect, row, col, isSelected)
+
 
     def GetBestSize(self, grid, attr, dc, row, col):
-        print 'Getting Best Size'
+        #print 'Getting Best Size'
         dc.SetFont(attr.GetFont())
-        w, h = dc.GetTextExtent('mmmmm')
+        w, h = dc.GetTextExtent('999.999 9999.999')
         return wx.Size(w, h)
 
     def Clone(self):
---
+++  tripleaxisproject/Polarization/pol_catalog2.py
@@ -0,0 +1,299 @@
+import wx
+import  wx.grid as  gridlib
+import sys,os
+import classify_files2 as classify_files
+import numpy as N
+import gridbar
+
+class MyApp(wx.App):
+    def __init__(self, redirect=False, filename=None, useBestVisual=False, clearSigInt=True):
+        wx.App.__init__(self,redirect,filename,clearSigInt)
+
+
+    def OnInit(self):
+        return True
+
+
+
+
+#---------------------------------------------------------------------------
+
+class CustomDataTable(gridlib.PyGridTableBase):
+    def __init__(self):
+        gridlib.PyGridTableBase.__init__(self)
+        self.colLabels = ['Select?', 'filename', 'polarization state','hsample','vsample','h','k','l','e','a3','a4','temp','magfield']
+        self.rowLabels=['Group 0']
+
+        self.dataTypes = [gridlib.GRID_VALUE_BOOL, #selected
+                          gridlib.GRID_VALUE_STRING,#filename
+                          gridlib.GRID_VALUE_STRING, #polarization state
+                          gridlib.GRID_VALUE_STRING, #hsample
+                          gridlib.GRID_VALUE_STRING, #vsample
+                          gridbar.GRID_VALUE_BAR, #h
+                          gridbar.GRID_VALUE_BAR, #k
+                          gridbar.GRID_VALUE_BAR, #l
+                          gridbar.GRID_VALUE_BAR, #e
+                          gridbar.GRID_VALUE_BAR, #a3
+                          gridbar.GRID_VALUE_BAR, #a4
+                          gridbar.GRID_VALUE_BAR, #temp
+                          gridbar.GRID_VALUE_BAR, #magfield
+                          #gridlib.GRID_VALUE_STRING,
+                          ]
+        self.data = []
+        self.data.append([1, #selected
+                        '', #filename
+                        '', #polarization state
+                        '', #hsample
+                        '', #vsample
+                        gridbar.Bar(5,6,(0,10)), #h
+                        gridbar.Bar(5,7,(0,10)), #k
+                        gridbar.Bar(5,8,(0,10)), #l
+                        gridbar.Bar(5,9,(0,10)), #e
+                        gridbar.Bar(5,10,(0,10)), #a3
+                        gridbar.Bar(5,11,(0,10)), #a4
+                        gridbar.Bar(5,12,(0,10)), #temp
+                        gridbar.Bar(5,13,(0,10)), #magfield
+                        ])
+        return
+            #[1010, "The foo doesn't bar", "major", 1, 'MSW', 1, 1, 1, 1.12],
+            #[1011, "I've got a wicket in my wocket", "wish list", 2, 'other', 0, 0, 0, 1.50],
+            #[1012, "Rectangle() returns a triangle", "critical", 5, 'all', 0, 0, 0, 1.56]
+            #]
+    #--------------------------------------------------
+    # required methods for the wxPyGridTableBase interface
+    def GetNumberRows(self):
+        return len(self.data) + 0
+    def GetNumberCols(self):
+        return len(self.colLabels)
+    def IsEmptyCell(self, row, col):
+        try:
+            return not self.data[row][col]
+        except IndexError:
+            return True
+    # Get/Set values in the table.  The Python version of these
+    # methods can handle any data-type, (as long as the Editor and
+    # Renderer understands the type too,) not just strings as in the
+    # C++ version.
+    def GetValue(self, row, col):
+        try:
+            return self.data[row][col]
+        except IndexError:
+            return ''
+    def SetValue(self, row, col, value):
+        try:
+            self.data[row][col] = value
+        except IndexError:
+            # add a new row
+            self.data.append([''] * self.GetNumberCols())
+            self.rowLabels.append('Group '+str(len(self.rowLabels)))
+            #print 'setting row ',row,' col ',col, ' val ',value
+            #print self.__dict__
+            #self.SetValue(row, col, value)
+            self.data[row][col] = value
+            #print 'set'
+
+            # tell the grid we've added a row
+            msg = gridlib.GridTableMessage(self,            # The table
+                    gridlib.GRIDTABLE_NOTIFY_ROWS_APPENDED, # what we did to it
+                    1                                       # how many
+                    )
+            self.GetView().ProcessTableMessage(msg)
+        return
+    #--------------------------------------------------
+    # Some optional methods
+    # Called when the grid needs to display labels
+    def GetColLabelValue(self, col):
+        return self.colLabels[col]
+    # Called when the grid needs to display labels
+    def GetRowLabelValue(self, row):
+        return self.rowLabels[row]
+    # Called to determine the kind of editor/renderer to use by
+    # default, doesn't necessarily have to be the same type used
+    # natively by the editor/renderer if they know how to convert.
+    def GetTypeName(self, row, col):
+        return self.dataTypes[col]
+    # Called to determine how the data can be fetched and stored by the
+    # editor and renderer.  This allows you to enforce some type-safety
+    # in the grid.
+    def CanGetValueAs(self, row, col, typeName):
+        colType = self.dataTypes[col].split(':')[0]
+        if typeName == colType:
+            return True
+        else:
+            return False
+    def CanSetValueAs(self, row, col, typeName):
+        return self.CanGetValueAs(row, col, typeName)
+#---------------------------------------------------------------------------
+class CustTableGrid(gridlib.Grid):
+    def __init__(self, parent):
+        gridlib.Grid.__init__(self, parent, -1)
+        table = CustomDataTable()
+
+        gridbar.register(self)
+        # The second parameter means that the grid is to take ownership of the
+        # table and will destroy it when done.  Otherwise you would need to keep
+        # a reference to it and call it's Destroy method later.
+        self.SetTable(table, True)
+        #attr = gridlib.GridCellAttr()
+        #attr.SetReadOnly(True)
+        #attr.SetRenderer(gridbar.GridCellBarRenderer())
+        #self.SetColAttr(13, attr)
+        #self.SetCellValue(1,13,'q')
+        #self.SetCellRenderer(1,13,gridbar.GridCellBarRenderer)
+        #self.SetRowLabelSize(0)
+        self.SetMargins(0,0)
+        self.AutoSize()
+        #gridlib.Grid.SetSelectionMode(self,gridlib.Grid.SelectRows)
+        gridlib.Grid.EnableEditing(self,False)
+        gridlib.EVT_GRID_CELL_LEFT_DCLICK(self, self.OnLeftDClick)
+    # I do this because I don't like the default behaviour of not starting the
+    # cell editor on double clicks, but only a second click.
+    def OnLeftDClick(self, evt):
+        if self.CanEnableCellControl():
+            self.EnableCellEditControl()
+
+
+class CatalogFrame(wx.Frame):
+    def __init__(self,parent,id):
+        wx.Frame.__init__(self,parent,id,'File Catalog',size=(640,200))
+        self.Bind(wx.EVT_CLOSE,self.OnCloseWindow)
+        self.createMenuBar()
+        p = wx.Panel(self, -1, style=0)
+        grid = CustTableGrid(p)
+        bs = wx.BoxSizer(wx.VERTICAL)
+        bs.Add(grid, 1, wx.GROW|wx.ALL|wx.EXPAND, 5)
+        p.SetSizer(bs)
+        #p.Fit()
+        self.grid=grid
+
+    def menuData(self):
+        return(("&File",\
+                ("&Open","Open",self.OnOpen),\
+                ("&Quit","Quit",self.OnCloseWindow)),\
+                )
+
+    def createMenuBar(self):
+        menuBar=wx.MenuBar()
+        for eachMenuData in self.menuData():
+            menuLabel=eachMenuData[0]
+            menuItems=eachMenuData[1:]
+            menuBar.Append(self.createMenu(menuItems),menuLabel)
+            self.SetMenuBar(menuBar)
+
+    def createMenu(self,menuData):
+        menu=wx.Menu()
+        for eachLabel, eachStatus, eachHandler in menuData:
+            if not eachLabel:
+                menu.AppendSeparator()
+                continue
+            menuItem=menu.Append(-1,eachLabel,eachStatus)
+            self.Bind(wx.EVT_MENU,eachHandler,menuItem)
+        return menu
+
+    def OnCloseWindow(self,event):
+        self.Destroy()
+
+    def OnOpen(self,event):
+        # Create the dialog. In this case the current directory is forced as the starting
+        # directory for the dialog, and no default file name is forced. This can easilly
+        # be changed in your program. This is an 'open' dialog, and allows multitple
+        # file selections as well.
+        #
+        # Finally, if the directory is changed in the process of getting files, this
+        # dialog is set up to change the current working directory to the path chosen.
+
+        defaultDir=os.getcwd()
+        defaultDir=r'c:\bifeo3xtal\jan8_2008\9175'
+        wildcard="bt7 files (*.bt7)|*.bt7|All files (*.*)|*.*"
+        dlg = wx.FileDialog(
+            self, message="Choose a file",
+            defaultDir=defaultDir,
+            defaultFile="",
+            wildcard=wildcard,
+            style=wx.OPEN | wx.MULTIPLE | wx.CHANGE_DIR
+            )
+
+        # Show the dialog and retrieve the user response. If it is the OK response,
+        # process the data.
+        if dlg.ShowModal() == wx.ID_OK:
+            # This returns a Python list of files that were selected.
+            paths = dlg.GetPaths()
+
+            #self.log.WriteText('You selected %d files:' % len(paths))
+            self.files=paths
+            #for path in paths:
+            #    self.log.WriteText('           %s\n' % path)
+
+        # Compare this with the debug above; did we change working dirs?
+        #self.log.WriteText("CWD: %s\n" % os.getcwd())
+
+
+        self.catalog=classify_files.readfiles(self.files)
+        #print 'opening:'
+        #print self.catalog.pm.files
+        table=self.grid.GetTable()
+        for row in range(len(self.catalog.files)):
+            gridlib.Grid.SetCellValue(self.grid,row+1,1,self.catalog.files[row])
+            gridlib.Grid.SetCellValue(self.grid,row+1,2,str(self.catalog.data[row]['polarization state']))
+            gridlib.Grid.SetCellValue(self.grid,row+1,3,str(self.catalog.data[row]['hsample']))
+            gridlib.Grid.SetCellValue(self.grid,row+1,4,str(self.catalog.data[row]['vsample']))
+            if self.catalog.data[row].has_key('h'):
+                range_column=(self.catalog.h_range.min,self.catalog.h_range.max)
+                range_cell=(self.catalog.data[row]['h']['min'],self.catalog.data[row]['h']['max'])
+                currbar=gridbar.Bar(self.catalog.data[row]['h']['min'],self.catalog.data[row]['h']['max'],range_column)
+                table.SetValue(row+1,5,currbar)
+            if self.catalog.data[row].has_key('k'):
+                range_column=(self.catalog.k_range.min,self.catalog.k_range.max)
+                range_cell=(self.catalog.data[row]['k']['min'],self.catalog.data[row]['k']['max'])
+                currbar=gridbar.Bar(self.catalog.data[row]['k']['min'],self.catalog.data[row]['k']['max'],range_column)
+                table.SetValue(row+1,6,currbar)
+            if self.catalog.data[row].has_key('l'):
+                range_column=(self.catalog.l_range.min,self.catalog.l_range.max)
+                range_cell=(self.catalog.data[row]['l']['min'],self.catalog.data[row]['l']['max'])
+                currbar=gridbar.Bar(self.catalog.data[row]['l']['min'],self.catalog.data[row]['l']['max'],range_column)
+                table.SetValue(row+1,7,currbar)
+            if self.catalog.data[row].has_key('e'):
+                range_column=(self.catalog.e_range.min,self.catalog.e_range.max)
+                range_cell=(self.catalog.data[row]['e']['min'],self.catalog.data[row]['e']['max'])
+                currbar=gridbar.Bar(self.catalog.data[row]['e']['min'],self.catalog.data[row]['e']['max'],range_column)
+                table.SetValue(row+1,8,currbar)
+            if self.catalog.data[row].has_key('a3'):
+                range_column=(self.catalog.a3_range.min,self.catalog.a3_range.max)
+                range_cell=(self.catalog.data[row]['a3']['min'],self.catalog.data[row]['a3']['max'])
+                currbar=gridbar.Bar(self.catalog.data[row]['a3']['min'],self.catalog.data[row]['a3']['max'],range_column)
+                table.SetValue(row+1,9,currbar)
+            if self.catalog.data[row].has_key('a4'):
+                range_column=(self.catalog.a4_range.min,self.catalog.a4_range.max)
+                range_cell=(self.catalog.data[row]['a4']['min'],self.catalog.data[row]['a4']['max'])
+                currbar=gridbar.Bar(self.catalog.data[row]['a4']['min'],self.catalog.data[row]['a4']['max'],range_column)
+                table.SetValue(row+1,10,currbar)
+            if self.catalog.data[row].has_key('temp'):
+                print 'temp'
+                range_column=(self.catalog.temp_range.min,self.catalog.temp_range.max)
+                range_cell=(self.catalog.data[row]['temp']['min'],self.catalog.data[row]['temp']['max'])
+                currbar=gridbar.Bar(self.catalog.data[row]['temp']['min'],self.catalog.data[row]['temp']['max'],range_column)
+                table.SetValue(row+1,11,currbar)
+            if self.catalog.data[row].has_key('magfield'):
+                range_column=(self.catalog.magfield_range.min,self.catalog.magfield_range.max)
+                range_cell=(self.catalog.data[row]['magfield']['min'],self.catalog.data[row]['magfield']['max'])
+                currbar=gridbar.Bar(self.catalog.data[row]['magfield']['min'],self.catalog.data[row]['magfield']['max'],range_column)
+                table.SetValue(row+1,12,currbar)
+
+
+
+        gridlib.Grid.AutoSize(self.grid)
+        gridlib.Grid.ForceRefresh(self.grid)
+        # Destroy the dialog. Don't do this until you are done with it!
+        # BAD things can happen otherwise!
+        dlg.Destroy()
+
+
+
+
+
+
+if __name__=='__main__':
+    app=MyApp()
+    frame=CatalogFrame(parent=None,id=-1)
+    frame.Show()
+    app.MainLoop()
