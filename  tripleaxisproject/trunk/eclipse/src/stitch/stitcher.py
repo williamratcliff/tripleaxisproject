8c1a564f6e3f140a17f28acb97ce5590ba99dce2
william.ratcliff <william.ratcliff@e28a235e-f944-0410-a937-4d0c1e564b32>
2008-05-31 19:01:19 +0000

    stitching is almost there!

---  tripleaxisproject/trunk/eclipse/src/stitch/stitcher.py
+++  tripleaxisproject/trunk/eclipse/src/stitch/stitcher.py
@@ -4,7 +4,7 @@ from utilities import  scriptutil as SU
 import re,os,sys
 from utilities import readncnr3 as readncnr
 import copy
-from reflectometry.reduction import rebin
+from reflectometry.reduction.rebin import rebin
 pi=N.pi
 
 
@@ -15,21 +15,7 @@ def cshift(l, offset):
    offset %= len(l)
    return N.concatenate((l[-offset:], l[:-offset]))
 
-def rebin(x,I,xo,Io=None):
-    """
-    Rebin a vector.
-    
-    x are the existing bin edges
-    xo are the new bin edges
-    I are the existing counts (one fewer than edges)
-    
-    Io will be used if present, but be sure that it is a contiguous
-    array of the correct shape and size.
-    """
-    x,I,xo = [_input(v) for v in (x,I,xo)]
-    Io = _output(Io,[len(xo)-1])
-    _reduction.rebin(x,I,xo,Io)
-    return Io
+
 
 class Psd:
     def __init__(self,left=0,center=21,right=48):
@@ -50,34 +36,87 @@ class Stitch:
         self.a4_begin=self.a4[0]-ch_space[psd.left]
         self.a4_end=self.a4[-1]-ch_space[psd.right]
         self.output_npts=int(N.round(N.absolute(self.a4_end-self.a4_begin)/self.outputwidth))
-        self.output_a4=N.arange(N.min([self.a4_begin,self.a4_end]),N.max([self.a4_begin,self.a4_end]),outputwidth)
+        self.output_a4=N.arange(N.min([self.a4_begin,self.a4_end]),N.max([self.a4_begin,self.a4_end])+outputwidth,outputwidth)
         
         print self.output_npts
-        print self.output_a4
+        print self.output_a4.shape
+        print self.a4_begin
         print self.a4_end        
         detectors=self.data.metadata['count_info']['AnalyzerDetectorDevicesOfInterest'.lower()]
         self.corrected=False
         self.counts_ending=''
         self.errors_ending=''
+        self.detectors=detectors
         if data.data.has_key(detectors[0]+'_corrected'):
             self.corrected=True
             self.counts_ending='_corrected'
             self.errors_ending='_errs_corrected'
         i=0
-        z_in={}
-        dz_in={}
+        z_in=[]
+        dz_in=[]
         for detector in detectors:
-            z_in[detector]=N.array(self.data.data[detector+self.counts_ending])*ch_eff[i]
-            dz_in[detector]=N.array(self.data.data[detector+self.errors_ending])*ch_eff[i]
+            z_in.append(N.array(self.data.data[detector+self.counts_ending])*ch_eff[i])
+            if self.errors_ending!='':
+                dz_in.append(N.array(self.data.data[detector+self.errors_ending])*ch_eff[i])
+            else:
+                dz_in.append(N.sqrt(N.array(self.data.data[detector+self.counts_ending]))*ch_eff[i])
+        self.data_eff=N.array(z_in).T
+        self.data_err_eff=N.array(dz_in).T
         
+        print self.data_eff.shape
+        #print z_in.shape
         
         
     def stitch(self):
         data=self.data
+        #data_plus_count=N.zeros(self.output_a4.shape,'float64')
+        mon_in=N.ones(self.ch_eff.shape,'float64')
+        i=0
+        ch_eff=self.ch_eff
+        for detector in self.detectors:
+            if ch_eff[i] < 1e-2:
+                mon_in[i]=0
+            i=i+1
+        #output_tmp=N.zeros(self.output_a4.shape,'float64')
+        #dz_output_tmp=N.zeros(self.output_a4.shape,'float64')
+        #output_mon=N.zeros(self.output_a4.shape,'float64')
+        output_data=0.0*self.output_a4[0:-1]
+        output_data_err=N.zeros(output_data.shape,'float64')
+        data_norm=N.ones(output_data.shape,'float64')
         for i in range(self.a4.shape[0]):
             ch_boundary=self.ch_boundary+self.a4[i]
+            z_in = self.data_eff[i,:]*ch_eff
+            dz_in = self.data_err_eff[i,:]*ch_eff
+            #print z_in
+            #print dz_in
+            ch_boundary=N.flipud(ch_boundary)
+            z_in=N.flipud(z_in)
+            dz_in=N.flipud(dz_in)
+            #;output_data_left=reverse(output_data_left)
+            output_tmp=N.array(rebin(ch_boundary[self.psd.left:self.psd.right+1].tolist(),z_in[self.psd.left:self.psd.right].tolist(),self.output_a4.tolist()))
+            dz_output_tmp=N.array(rebin(ch_boundary[self.psd.left:self.psd.right+1].tolist(),dz_in[self.psd.left:self.psd.right].tolist(),self.output_a4.tolist()))
+            output_mon=N.array(rebin(ch_boundary[self.psd.left:self.psd.right+1].tolist(),mon_in[self.psd.left:self.psd.right].tolist(),self.output_a4.tolist()))
+            #drebin,ch_left[range[0]:range[2]+1],z_in[range[0]:range[2]],dz_in[range[0]:range[2]],output_data_left,output_tmp,dz_output_tmp,/histogram,/to_histogram,err=err,emsg=emsg
             
-        
+            
+            #print, emsg
+            #drebin,ch_left[range[0]:range[2]+1],mon_in[range[0]:range[2]],mon_in[range[0]:range[2]],output_data_left,output_mon,dz_output_mon,/histogram,/to_histogram,err=err,emsg=emsg
+            #print output_data.shape
+            #print output_tmp#.shape
+            #print output_mon.shape
+            #print dz_output_tmp.shape
+            #print output_tmp^2
+            output_data=output_data+output_tmp*output_mon
+            output_data_err=output_data_err+dz_output_tmp**2*output_mon**2
+            data_norm=data_norm+output_mon
+        self.output_data=output_data/data_norm
+        self.output_data_err=N.sqrt(output_data_err)/data_norm
+        print 'norm'
+        #print data_norm.shape
+        #print output_mon.shape
+        #print dz_output_tmp.shape
+        #print output_data.shape
+        #print output_tmp.shape
         
 
 
@@ -115,7 +154,12 @@ if __name__=='__main__':
         mydatareader=readncnr.datareader()
         mydata=mydatareader.readbuffer(myfilestr)
         mystitcher=Stitch(mydata,ch_space,ch_boundary,ch_eff,mypsd)
-        outdata=mystitcher.stitch()
+        mystitcher.stitch()
+        print mystitcher.output_a4.shape
+        print mystitcher.output_data.shape
+        #pylab.plot(mystitcher.output_a4[0:-1],mystitcher.output_data)
+        pylab.errorbar(mystitcher.output_a4[0:-1],mystitcher.output_data,mystitcher.output_data_err,marker='s',linestyle='None',mfc='black',mec='black',ecolor='black')
+        pylab.show()
         
         
         
