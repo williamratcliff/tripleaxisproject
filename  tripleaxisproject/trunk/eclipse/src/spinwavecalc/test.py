222b35364e19e1d815c0864dcf6ac22cf4803d9c
william.ratcliff <william.ratcliff@e28a235e-f944-0410-a937-4d0c1e564b32>
2008-07-29 21:10:03 +0000

    started spinwave calculations

---
+++  tripleaxisproject/trunk/eclipse/src/spinwavecalc/test.py
@@ -0,0 +1,88 @@
+import sympy
+import numpy as N
+
+
+class atom:
+    def __init__(self,spin=[0,0,1],pos=[0,0,0],neighbors=[],interactions=[],label=0,Dx=0,Dy=0,Dz=0):
+        self.spin=spin
+        self.pos=pos
+        self.neighbors=neighbors
+        self.interactions=interactions
+        self.label=label
+        self.Dx=Dx
+        self.Dy=Dy
+        self.Dz=Dz
+
+def generate_sabn(N_atoms):
+    Sabn=[]
+    S=sympy.Symbol("S")
+    for i in range(N_atoms):
+        c=sympy.Symbol("c%d"%(i,),commutative=False)
+        cd=sympy.Symbol("cd%d"%(i,),commutative=False)
+        curr=[sympy.sqrt(S/2)*(c+cd),sympy.sqrt(S/2)*(c-cd)/sympy.I,S-cd*c]
+        Sabn.append(curr)
+    return Sabn
+
+def generate_sxyz(N_atoms):
+    Sxyz=[]
+    for i in range(N_atoms):
+        S=sympy.Symbol("Sxyz%d"%(i,),commutative=False)
+        Sxyz.append(S)
+    return Sxyz
+
+def generate_hdef(atom_list,Jij,Sxyz):
+    N_atoms=len(atom_list)
+    Hdef=0
+    print 'atom_list',atom_list
+    print 'Sxyz',Sxyz
+    print 'Jij',Jij
+    for i in range(N_atoms):
+        N_int=len(atom_list[i].interactions)
+        for j in range(N_int):
+            print Jij[atom_list[i].interactions[j]]
+            Hij=Sxyz[i]*Jij[atom_list[i].interactions[j]]*Sxyz[atom_list[i].neighbors[j]]
+            Hij=Hij-atom_list[i].Dx*Sxyz[i]**2-atom_list[i].Dy*Sxyz[i][1]**2-atom_list[i].Dz*Sxyz[i][2]**2
+            Hdef=Hdef+Hij
+    return Hdef
+
+def generate_atoms():
+    spin0=[0,0,1]
+    pos0=[0,0,0]
+    neighbors=[1]
+    interactions=[0]
+    atom0=atom(spin=spin0,pos=pos0,neighbors=neighbors,interactions=interactions,label=0)
+    pos0=[0,0.5,0]
+    neighbors=[0]
+    interactions=[0]
+    atom1=atom(spin=spin0,pos=pos0,neighbors=neighbors,interactions=interactions,label=1)
+    atomlist=[atom0,atom1]
+    return atomlist
+   
+    
+if __name__=='__main__':
+
+    if 0:
+        A = sympy.Symbol("A", commutative=False)
+        B = sympy.Symbol("B", commutative=False)
+        e = (A+B)**2
+        g=e.expand()
+        l=g.subs('A*B',1)
+        print l
+    if 0:
+        x = sympy.Symbol('x')
+        p = sympy.Wild('p',exclude='x')
+        q = sympy.Wild('q',exclude='x')
+        myexp=(5*x**2 + 3*x)
+        r=myexp.match(p*x**2+q*x )
+        print r
+        print 1./20
+    if 1:
+        N_atoms=3
+        Sabn=generate_sabn(N_atoms)
+        print Sabn[0]
+        Sxyz=generate_sxyz(N_atoms)
+        atom_list=generate_atoms()
+        Jij=[N.matrix([[1,0,0],[0,1,0],[0,0,1]])]
+        Hdef=generate_hdef(atom_list,Jij,Sxyz)
+        print Hdef
+        
