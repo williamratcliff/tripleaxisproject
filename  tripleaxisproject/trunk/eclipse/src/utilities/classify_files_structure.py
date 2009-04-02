8a2a03fa5f4c51f631db70049fa33b44bd603cdb
william.ratcliff <william.ratcliff@e28a235e-f944-0410-a937-4d0c1e564b32>
2009-04-02 20:28:41 +0000

    started working on classifying files

---
+++  tripleaxisproject/trunk/eclipse/src/utilities/classify_files_structure.py
@@ -0,0 +1,33 @@
+import readncnr3 as readncnr
+import numpy as N
+
+
+
+
+if __name__=='__main__':
+    myfilestr=r'C:\Ce2RhIn8\Mar10_2009\magsc035.bt9'
+    #myfilestr=r'c:\bifeo3xtal\jan8_2008\9175\fpx53418.bt7'
+    #myfilestr=r'c:\13165\13165\data\MagHigh56784.bt7'
+    #myfilestr=r'c:\13176\data\CeOFeAs57255.bt7.out'
+    mydatareader=readncnr.datareader()
+    mydata=mydatareader.readbuffer(myfilestr)
+    print mydata.__dict__
+    print mydata.additional_metadata
+    print mydata.metadata
+    print mydata.metadata['file_info']['scantype']
+    print mydata.metadata['collimations']
+    print mydata.metadata['dspacing']['monochromator_dspacing']
+    print mydata.metadata['dspacing']['analyzer_dspacing']
+    print mydata.metadata['lattice']['a']
+    print mydata.metadata['lattice']['b']
+    print mydata.metadata['lattice']['c']
+    print mydata.metadata['lattice']['alpha']
+    print mydata.metadata['lattice']['beta']
+    print mydata.metadata['lattice']['gamma']
+    print mydata.metadata['motor3']['step']
+    print mydata.metadata['motor4']['step']
+    print mydata.metadata['count_info']['monitor']
+    print mydata.metadata['energy_info']
+    print mydata.metadata['q_center']
+    
+    
