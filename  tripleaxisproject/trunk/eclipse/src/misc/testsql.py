1d26c5150678aebbbd1a65794d1f4ca05fbffb9f
william.ratcliff <william.ratcliff@e28a235e-f944-0410-a937-4d0c1e564b32>
2009-06-03 04:17:49 +0000

    played a bit more with testsql

---  tripleaxisproject/trunk/eclipse/src/misc/testsql.py
+++  tripleaxisproject/trunk/eclipse/src/misc/testsql.py
@@ -1,16 +1,20 @@
 import sqlalchemy
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, DateTime,BLOB, Text
 import inspect
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relation, backref
+from sqlalchemy.orm import relation, backref, sessionmaker
+import datetime
 
 Base=declarative_base()
 
-engine = create_engine('mysql://root:secret@localhost/sampleinventory')
+#engine = create_engine('mysql://root:secret@localhost/sampleinventory')
+engine = create_engine('mysql://root:secret@localhost/sampleinventory?charset=utf8&use_unicode=0', pool_recycle=3600)
 metadata = MetaData()
 meta.bind = engine
+Session=sessionmaker(bind=engine)
+session=Session()
 
-class Instrument(Base):
+class Instruments(Base):
     __tablename__='instruments'
     __table_args__ = {'mysql_engine':'InnoDB'}
     id=Column(Integer, primary_key=True)
@@ -20,14 +24,25 @@ class Instrument(Base):
     def __repr__(self):
         return "<Instrument('%s')>"%(self.name,)
 
-class Sample(Base):
+class Samples(Base):
     __tablename__='samples'
     __table_args__ = {'mysql_engine':'InnoDB'}
     id=Column(Integer, primary_key=True)
-    name=Column(String, nullable=False)
-    def __init__(sample_name, chemical_formula, quantity, sample_type, hazards, current_location, 
+    name=Column(String(250), nullable=False)
+    date_shipped=Column(DateTime)
+    #form=Column(String(50))
+    quantity=Column(String(50))
+    MSDS=Column(Text)
+    hazards=Column(String(250))
+    current_location=Column(Text)
+    sample_type=Column(String(50))
+    chemical_formula=Column(String(50))
+    previous_id=Column(Integer)
+    
+    comments=Column(Text)
+    def __init__(self,sample_name, chemical_formula, quantity, sample_type, hazards, current_location, 
                  comments=None, MSDS=None, date_shipped=None):
-        self.sample_name=sample_name
+        self.name=sample_name #eventually a barcode
         self.chemical_formula=chemical_formula
         self.quantity=quantity 
         self.sample_type=sample_type #(powder, crystal, liquid)
@@ -36,6 +51,53 @@ class Sample(Base):
         self.comments=comments
         self.MSDS=MSDS
         self.date_shipped=date_shipped #should be a date
+
+class Photos(Base):
+    __tablename__='photos'
+    __table_args__ = {'mysql_engine':'InnoDB'}
+    id=Column(Integer, primary_key=True)
+    name=Column(String, nullable=False)
+    sample_id=Column(Integer, ForeignKey('samples.id'))
+    sample=relation(Samples,backref=backref('photos'))
+    experiment=relation('Experiments',secondary=photos_experiments, backref='photos')
+    comments=Column(Text)
+    def __init__(self,photo):
+        self.photo=photo
+
+photos_experiments=Table('photos_experiment',metadata,
+                          Column('photo_id',Integer,ForeignKey('photos.id')),
+                          Column('experiment_id',Integer,ForeignKey('experiments.id'))
+                          )        
+
+
+class Experiments(object):
+    __tablename__='experiments'
+    __table_args__ = {'mysql_engine':'InnoDB'}
+    id=Column(Integer, primary_key=True)
+    name=Column(String(250), nullable=False) #Title from Webpage
+    proposal_id=Column(Integer,nullable=False)
+    date_on=Column(DateTime)
+    date_off=Column(DateTime)
+    flux=Column(Integer)
+    comments=Column(Text)
+    sample_id=Column(Integer, ForeignKey('samples.id'))
+    instrument_id=Column(Integer, ForeignKey('experiments.id'))
+    
+    
+    def __init__(date_on, date_off, flux, instrument_id, sample_id, comments=''):
+        self.date_on=date_on
+        self.date_off=date_off
+        self.flux=flux
+        self.instrument_id=instrument_id
+        self.sample_id=sample_id
+        
+class PersonExperiment(object):
+    def __init__(person_id, experiment_id):
+        self.person_id=person_id
+        self.experiment_id=experiment_id
+        
+
+
 
 class Address(object):
     def __init__(self,address1=None,address2=None, country=None, zipcode=None, city=None, state=None):
@@ -121,38 +183,6 @@ class Person(Base):
         return "<Person('%s' ,'%s')>" % (self.first_name, self.last_name)        
         
         
-
-
-
-        
-
-        
-class Date(object):
-    def __init__(self,day,month, year):
-        self.day=day
-        self.month=month
-        self.year=year
-
-class Experiment(object):
-    def __init__(date_on, date_off, flux, instrument_id, sample_id, comments=''):
-        self.date_on=date_on
-        self.date_off=date_off
-        self.flux=flux
-        self.instrument_id=instrument_id
-        self.sample_id=sample_id
-        
-class PersonExperiment(object):
-    def __init__(person_id, experiment_id):
-        self.person_id=person_id
-        self.experiment_id=experiment_id
-        
-
-        
-
-        
-        
-
-
 
         
 
